583203a8a95f6daed70e880511644b0fc3125762
Dur4nt3 <189136258+Dur4nt3@users.noreply.github.com>
2025-03-19 23:41:10 +0200

    Expand "add tasks" modal creation logic

---
+++ src/modules/add-tasks-hybrid-monthly-section.js
@@ -0,0 +1,19 @@
+import { buildElement, hide, buildSelectElement, buildImgElement } from "./dom-manipulator.js";
+
+import infoSvg from "../images/More-information.svg";
+
+function repetitionTypeRow() {
+
+}
+
+function specificOccurrenceRow() {
+
+}
+
+function endOfMonthRow() {
+    
+}
+
+export function hybridMonthlyPatternSection(cont) {
+
+}
